6ebfac74526f586d1260397eed12ca81b2a3189c
Joaco Marina <joacomarina@MacBook-Pro-de-Jorge-Marina-3.local>
2024-04-15 16:47:02 -0300

    sql comentarios

--- proyectointegrador/db/data.sql
+++ proyectointegrador/db/data.sql
@@ -40,16 +40,20 @@ FOREIGN KEY (producto_id) REFERENCES productos(id),
 FOREIGN KEY (usuarios_id) REFERENCES usuarios(id)
 );
 
+/*SQL USUARIOS*/
+
 INSERT INTO usuarios (id, email, contrasenia, fecha, dni, fotoperfil)
-VALUES (DEFAULT, 'joacopro@gmail.com', '1234', '2004-12-16', '46293228', 'linkfotoperfil', DEFAULT, DEFAULT )
+VALUES (DEFAULT, 'joacopro@gmail.com', '1234', '2004-12-16', '46293228', 'usuario1foto', DEFAULT, DEFAULT )
 INSERT INTO usuarios (id, email, contrasenia, fecha, dni, fotoperfil)
-VALUES (DEFAULT, 'delfi@gmail.com', '1234', '2003-02-12', '41233248', 'linkfotoperfil', DEFAULT, DEFAULT )
+VALUES (DEFAULT, 'delfi@gmail.com', '1234', '2003-02-12', '41233248', 'usuario2foto', DEFAULT, DEFAULT )
 INSERT INTO usuarios (id, email, contrasenia, fecha, dni, fotoperfil)
-VALUES (DEFAULT, 'javi12@gmail.com', '1234', '2002-10-04', '34293448', 'linkfotoperfil', DEFAULT, DEFAULT )
+VALUES (DEFAULT, 'javi12@gmail.com', '1234', '2002-10-04', '34293448', 'usuario3foto', DEFAULT, DEFAULT )
 INSERT INTO usuarios (id, email, contrasenia, fecha, dni, fotoperfil)
-VALUES (DEFAULT, 'santiperez@gmail.com', '1234', '2006-03-16', '46293228', 'linkfotoperfil', DEFAULT, DEFAULT )
+VALUES (DEFAULT, 'santiperez@gmail.com', '1234', '2006-03-16', '46293228', 'usuario4foto', DEFAULT, DEFAULT )
 INSERT INTO usuarios (id, email, contrasenia, fecha, dni, fotoperfil)
-VALUES (DEFAULT, 'sofia19@gmail.com', '1234', '2004-12-16', '46293228', 'linkfotoperfil', DEFAULT, DEFAULT )
+VALUES (DEFAULT, 'sofia19@gmail.com', '1234', '2004-12-16', '46293228', 'usuario5foto', DEFAULT, DEFAULT )
+
+/*SQL PRODUCTOS*/
 
 INSERT INTO productos (id, usuarios_id, imagen_producto, descripcion_producto)
 VALUES (DEFAULT, 5, '', 'camiseta1', 'descripcion del producto')
@@ -72,11 +76,77 @@ VALUES (DEFAULT, 4, '', 'camiseta9', 'descripcion del producto')
 INSERT INTO productos (id, usuarios_id, imagen_producto, descripcion_producto)
 VALUES (DEFAULT, 5, '', 'camiseta10', 'descripcion del producto')
 
+/*SQL COMENTARIOS*/
 
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 1, 5, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 1, 3, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 1, 4, "comentario")
 
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 2, 1, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 2, 4, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 2, 4, "comentario")
 
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 3, 3, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 3, 1, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 3, 5, "comentario")
 
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 4, 1, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 4, 1, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 4, 2, "comentario")
 
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 5, 5, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 5, 3, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 5, 3, "comentario")
+
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 6, 2, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 6, 2, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 6, 3, "comentario")
+
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 7, 4, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 7, 1, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 7, 1, "comentario")
+
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 8, 2, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 8, 2, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 8, 5, "comentario")
+
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 9, 5, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 9, 5, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 9, 1, "comentario")
+
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 10, 5, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 10, 3, "comentario")
+INSERT INTO comentarios (id, producto_id, usuarios_id, comentario)
+VALUES (DEFAULT, 10, 4, "comentario")
 
 
 
