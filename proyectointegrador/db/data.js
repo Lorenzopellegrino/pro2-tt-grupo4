70fb3992fdbc99c86bb7f9ba0bafa560ae4baa91
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2024-04-15 11:43:11 -0300

    db

--- proyectointegrador/db/data.js
+++ proyectointegrador/db/data.js
@@ -1,18 +1,49 @@
 const info ={
     usuario: {
-        email:
-        usuario:
-        contrasenia:
-        fecha_de_nacimiento:
-        numero_documento:
-        foto
+        email: 'lolopelle1@gmail.com'
+        usuario: 'lolosoljoaco'
+        contrasenia:'0987654321'
+        fecha_de_nacimiento: '18/05/2005'
+        numero_documento:'52800016'
+        foto:
     },
-    productos:{[
-        
-    ],
-    comentarios:[
 
-    ]}
-    
+    productos:[
+        {
+        imagen:
+        nombre: 
+        foto:
+        comentarios:[
+            {
+                user:
+                comentario:
+                foto_de_perfil:
+            },
+            {
+                user:
+                comentario:
+                foto_de_perfil:
+            },
+            {
+                user:
+                comentario:
+                foto_de_perfil:
+            },
+            {
+                user:
+                comentario:
+                foto_de_perfil:
+            },
+            {
+                user:
+                comentario:
+                foto_de_perfil:
+            },
+        ]
+        },
+        {
 
-}
+        }
+    ]
+    }
+
