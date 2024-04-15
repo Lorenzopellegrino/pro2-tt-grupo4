bd2768d84b66b0e32174de8cd6a1ff2d31f9d9a2
Joaco Marina <joacomarina@MacBook-Pro-de-Jorge-Marina-3.local>
2024-04-15 15:36:57 -0300

    db e info

--- proyectointegrador/db/data.js
+++ proyectointegrador/db/data.js
@@ -1,331 +1,331 @@
 const info ={
     usuario: {
-        email: 'lolopelle1@gmail.com'
-        usuario: 'lolosoljoaco'
-        contrasenia:'0987654321'
-        fecha_de_nacimiento: '18/05/2005'
-        numero_documento:'52800016'
+        email: 'lolopelle1@gmail.com',
+        usuario: 'lolosoljoaco',
+        contrasenia:'0987654321',
+        fecha_de_nacimiento: '18/05/2005',
+        numero_documento:'52800016',
         foto:
     },
 
     productos:[
         {
-        imagen: 
-        nombre: 'Camiseta Barcelona'
-        descripcion: 'Camiseta del Barcelona de la temporada 2008/2009'
+        imagen: "https://www.thunderinternacional.com/cdn/shop/products/barcelona-titular-2009-messi-matchday-final-de-champions-979229.jpg?v=1613456732",
+        nombre: 'Camiseta Barcelona',
+        descripcion: 'Camiseta del Barcelona de la temporada 2008/2009',
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil:
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil:
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF6VASBa4XF7gnV8fbkNc0k8MsBOYripT4-8cj6-0ibw&s",
+        nombre: "Camiseta Barcelona",
+        descripcion: "Camiseta Barcelona de la temporada 2011",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3RHuV9BPh7ZDsryT54-pDR3jOvPygunmekPdWJ_kY2w&s",
+        nombre: "Camiseta Manchester City",
+        foto: "Camiseta Manchester City de la temporada 2016",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://m.media-amazon.com/images/I/61kzzrnV9fL._AC_SL1006_.jpg",
+        nombre: "Camiseta Real Madrid",
+        foto: "Camiseta Real Madrid 2015",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://www.thunderinternacional.com/cdn/shop/products/camiseta-adidas-de-river-plate-2018-19-frente_68122d40-14b9-42e8-8bae-831825fa772f.jpg?v=1681394469",
+        nombre: "Camiseta River",
+        foto: "Camiseta River de la temporada 2018",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://acdn.mitiendanube.com/stores/001/220/451/products/nike-jordan-xxxiii-2022-04-04t215806-2811-1098cb71fd288e298a16491210604442-640-0.png",
+        nombre: "Camiseta Boca Juniors",
+        foto: "Camiseta Boca Juniors de la temporada 2020",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFsKnpT74CVO-5zXqwCL1Y7xsTEeYTbTn4S3XU3z6j8w&s",
+        nombre: "Camiseta River",
+        foto: "Camiseta River de la temporada 2022",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_Xdl9IYJJ-TvFaoQXeYk-61wkTEF7uk0ATwRNFZJYTA&s",
+        nombre: "Camiseta Real Madrid",
+        foto: "Camiseta Real Madri de la temporada 2012",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWhfoMR9XjCHtcRRk01XmgBHjMo1XfCYSaqRTuuXB2jA&s",
+        nombre: "Camiseta Manchester City",
+        foto: "Camiseta Manchester City de la temporada 2020",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         }, 
         {
-        imagen:
-        nombre: 
-        foto:
+        imagen: "https://fotos.perfil.com/2019/09/01/velez-1994.jpg",
+        nombre: "Camiseta Velez",
+        foto: "Camiseta Velez de la temporada 1994",
         comentarios:[
             {
-                user: 'joacopro'
-                comentario: 'Gran producto, lo recomiendo'
-                foto_de_perfil: 
+                user: 'joacopro',
+                comentario: 'Gran producto, lo recomiendo',
+                foto_de_perfil: ,
             },
             {
-                user: 'luis_gomez'
-                comentario: 'Tarda mucho en llegar el envio?'
-                foto_de_perfil: 
+                user: 'luis_gomez',
+                comentario: 'Tarda mucho en llegar el envio?',
+                foto_de_perfil: ,
             },
             {
-                user: 'alfonsogimenez'
-                comentario: '10/10, lo probe y me encanto'
-                foto_de_perfil:
+                user: 'alfonsogimenez',
+                comentario: '10/10, lo probe y me encanto',
+                foto_de_perfil: ,
             },
             {
-                user: 'juan perez'
-                comentario: 'que buen producto'
-                foto_de_perfil:
+                user: 'juan perez',
+                comentario: 'que buen producto',
+                foto_de_perfil: ,
             },
             {
-                user: 'eugenia_ramirez'
-                comentario: 'No lo recomiendo'
-                foto_de_perfil:
+                user: 'eugenia_ramirez',
+                comentario: 'No lo recomiendo',
+                foto_de_perfil: ,
             },
         ]
         },   
