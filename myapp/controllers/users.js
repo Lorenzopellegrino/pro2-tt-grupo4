5848bd8df2013e5b94ec2f94605492953de998a5
Joaco Marina <joacomarina@MacBook-Pro-de-Jorge-Marina-3.local>
2024-04-16 12:56:07 -0300

    ejs

--- myapp/controllers/users.js
+++ myapp/controllers/users.js
@@ -2,16 +2,16 @@ const db = require('../db/data')
 
 const usercontroller = {
     login: function(req, res){
-        return res.send()
+        return res.render("login", {title: "LOGIN"})
     },
     register: function(req, res){
-        return res.send()
+        return res.render("register", {title: "REGISTER"})
     },
     profile: function(req, res){
-        return res.send()
+        return res.render("profile", {title: "PROFILE", usuario: db.usuarios, producto: db.productos})
     },
     edit: function(req, res){
-        return res.send()
+        return res.render("edit", {title: "EDIT", usuario: db.usuario})
     }
 };
 
