9adb226aaf13d149391f2011406a89a990168c33
solsejas <ssejas@udesa.edu.ar>
2024-04-16 18:39:12 -0300

    corrigiendo

--- myapp/controllers/indexcontroller.js
+++ myapp/controllers/indexcontroller.js
@@ -2,13 +2,13 @@ const db = require('../db/data')
 
 const indexcontroller = {
     index: function(req, res){
-        res.render('index', {title : 'Home', productos: db.productos})
+        return res.render('index', {title : 'Home', productos: db.productos})
     },
     search: function(req, res){
-        res.render('search-results', {title: 'search results', productos : db.productos})
+        return res.render('search-results', {title: 'search results', productos : db.productos})
     }
 };
 
 
-module.exports = indexcontroller
+module.exports = indexcontroller;
 
