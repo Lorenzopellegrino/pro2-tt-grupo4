1333e9cb2b9beec8a6d28815a7b7f1db13a088fb
solsejas <ssejas@udesa.edu.ar>
2024-04-17 13:13:10 -0300

    ejs y css

--- myapp/routes/product.js
+++ myapp/routes/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productoscontroller = require('../controllers/productoscontroller');
 
-router.get('/', productoscontroller.index);
+router.get('/product', productoscontroller.index);
 
 router.get('/add', productoscontroller.create);
 
