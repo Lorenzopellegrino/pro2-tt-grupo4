9a385a9c55c2097d306cbfb1af2a5ea42f6bd039
Joaco Marina <joacomarina@MacBook-Pro-de-Jorge-Marina-3.local>
2024-04-17 11:20:35 -0300

    ejs

--- myapp/db/data.js
+++ myapp/db/data.js
@@ -76,7 +76,7 @@ const info ={
         {
         imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3RHuV9BPh7ZDsryT54-pDR3jOvPygunmekPdWJ_kY2w&s",
         nombre: "Camiseta Manchester City",
-        foto: "Camiseta Manchester City de la temporada 2016",
+        descripcion: "Camiseta Manchester City de la temporada 2016",
         comentarios:[
             {
                 user: 'joacopro',
@@ -108,7 +108,7 @@ const info ={
         {
         imagen: "https://m.media-amazon.com/images/I/61kzzrnV9fL._AC_SL1006_.jpg",
         nombre: "Camiseta Real Madrid",
-        foto: "Camiseta Real Madrid 2015",
+        descripcion: "Camiseta Real Madrid 2015",
         comentarios:[
             {
                 user: 'joacopro',
@@ -140,7 +140,7 @@ const info ={
         {
         imagen: "https://www.thunderinternacional.com/cdn/shop/products/camiseta-adidas-de-river-plate-2018-19-frente_68122d40-14b9-42e8-8bae-831825fa772f.jpg?v=1681394469",
         nombre: "Camiseta River",
-        foto: "Camiseta River de la temporada 2018",
+        descripcion: "Camiseta River de la temporada 2018",
         comentarios:[
             {
                 user: 'joacopro',
@@ -172,7 +172,7 @@ const info ={
         {
         imagen: "https://acdn.mitiendanube.com/stores/001/220/451/products/nike-jordan-xxxiii-2022-04-04t215806-2811-1098cb71fd288e298a16491210604442-640-0.png",
         nombre: "Camiseta Boca Juniors",
-        foto: "Camiseta Boca Juniors de la temporada 2020",
+        descripcion: "Camiseta Boca Juniors de la temporada 2020",
         comentarios:[
             {
                 user: 'joacopro',
@@ -204,7 +204,7 @@ const info ={
         {
         imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFsKnpT74CVO-5zXqwCL1Y7xsTEeYTbTn4S3XU3z6j8w&s",
         nombre: "Camiseta River",
-        foto: "Camiseta River de la temporada 2022",
+        descripcion: "Camiseta River de la temporada 2022",
         comentarios:[
             {
                 user: 'joacopro',
@@ -236,7 +236,7 @@ const info ={
         {
         imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_Xdl9IYJJ-TvFaoQXeYk-61wkTEF7uk0ATwRNFZJYTA&s",
         nombre: "Camiseta Real Madrid",
-        foto: "Camiseta Real Madri de la temporada 2012",
+        descripcion: "Camiseta Real Madri de la temporada 2012",
         comentarios:[
             {
                 user: 'joacopro',
@@ -268,7 +268,7 @@ const info ={
         {
         imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWhfoMR9XjCHtcRRk01XmgBHjMo1XfCYSaqRTuuXB2jA&s",
         nombre: "Camiseta Manchester City",
-        foto: "Camiseta Manchester City de la temporada 2020",
+        descripcion: "Camiseta Manchester City de la temporada 2020",
         comentarios:[
             {
                 user: 'joacopro',
@@ -300,7 +300,7 @@ const info ={
         {
         imagen: "https://fotos.perfil.com/2019/09/01/velez-1994.jpg",
         nombre: "Camiseta Velez",
-        foto: "Camiseta Velez de la temporada 1994",
+        descripcion: "Camiseta Velez de la temporada 1994",
         comentarios:[
             {
                 user: 'joacopro',
