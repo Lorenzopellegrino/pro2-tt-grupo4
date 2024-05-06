c78861267ac8e6b191bef6e883cc5f03ca75294f
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2024-05-06 17:39:41 -0300

    sequilize

---
+++ myapp/database/models/Producto.js
@@ -0,0 +1,34 @@
+const funcionProducto = function (sequelize, dataTypes) {
+    let alias = "Producto";
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        producto_id: {
+            type: dataTypes.INTEGRER
+        },
+        usuario_id: {
+            type: dataTypes.INTEGRER
+        },
+        comentario: {
+            type: dataTypes.STRING
+        }
+    };
+    
+    let config = {
+        tableName: "comentarios",
+        timestamps: false,
+        underscored: true
+    };
+
+        /* definir un modelo */
+
+    let Comentario = sequelize.define(alias, cols, config);
+
+    return Comentario;
+}
+
+module.exports = funcionComentario
