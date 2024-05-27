3338c5a4207eaf4ed29dc8ffdffbe630fae119ab
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2024-05-27 17:01:29 -0300

    session

--- myapp/app.js
+++ myapp/app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
+const session = require ('express-session');
 
 var app = express();
 
@@ -23,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/product', productRouter);
+app.use(session({
+  secret: 'myapp',
+  resave: false,
+  saveUninitialized : true}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
