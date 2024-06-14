414a8e160e315ce2d61728ed3afa79f8d9b27a14
Joaco Marina <joacomarina@MacBook-Pro-de-Jorge-Marina-3.local>
2024-06-14 11:59:59 -0300

    error controllers

--- myapp/controllers/productoscontroller.js
+++ myapp/controllers/productoscontroller.js
@@ -2,13 +2,16 @@ const db = require('../db/data');
 
 const productoContoller = {
     index: function(req, res){
+        
         let id = req.params.id;
+
         let criterio = {
             include: [
             {association: "comentarios"},
             {association: "usuario"}
             ]
         }
+        
         db.productos.findByPk(id, criterio)
         .then(function (results) {
            return res.send("OK")
@@ -17,8 +20,8 @@ const productoContoller = {
             console.log(respuestaNegativa);
         })
         // return res.render('product', {title : 'Detalle Producto' , productos : db.productos})
-
     },
+
     add: function(req, res){
 
         db.usuario.findOne()
