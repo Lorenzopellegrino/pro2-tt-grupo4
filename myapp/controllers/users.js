c95294644f4bc974d8fac7ebc4faff9938ff7f51
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2024-06-19 11:37:56 -0300

    login y header

--- myapp/controllers/users.js
+++ myapp/controllers/users.js
@@ -47,6 +47,7 @@ const usercontroller = {
 
     loginUser: function(req,res) {
         let form = req.body;
+        let errors = validationResult(req);
 
         let filtro = {
             where: [{email:form.email}]
@@ -55,19 +56,12 @@ const usercontroller = {
         db.Usuario.findOne(filtro)
         .then((result) => {
             if (result != null){
-                let check = bcrypt.compareSync(form.password,result.password);
-
-                if (check){
-                    req.session.user = result;
-                    if (form.rememberme != undefined){
-                        res.cookie("userId", result.id,{maxAge: 1000 * 60 * 35})
-                    }
-                    return res.redirect("/users/profile");
-                }else {
-                    return res.send("error en la contrasenia");
-                }
-            }else {
-                return res.send ("No hay email parecidos a :" + form.email);
+                req.session.usuario = result;
+                return res.redirect("/")
+                
+            }
+            else{
+                return res.send("No se a encontrado el mail :" + form.email)
             }
         }).catch((err) => {
             return console.log(err);
