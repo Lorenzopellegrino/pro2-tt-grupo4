84c265a569721ed6108c40420b4c847879d79085
Joaco Marina <joacomarina@MacBook-Pro-de-Jorge-Marina-3.local>
2024-06-21 20:48:37 -0300

    productos controller + rutas de product

--- myapp/controllers/productoscontroller.js
+++ myapp/controllers/productoscontroller.js
@@ -1,4 +1,6 @@
 const db = require('../database/models');
+const op = db.Sequelize.Op;
+const {validationResult} = require("express-validator")
 
 const productocontoller = {
     index: function(req, res){
@@ -7,33 +9,146 @@ const productocontoller = {
 
         let criterio = {
             include: [
-            {association: "comentarios"},
-            {association: "usuario"}
-            ]
-        }
+            {association: "usuario"},
+            {association: "comentarios",
+                include: [{association: "usuario"}
+            ]}
+        ],
+        order: [
+            ["comentario", "createdAt", "DESC"]
+        ]
+        };
 
-        db.productos.findByPk(id, criterio)
+        db.Producto.findByPk(id, criterio)
         .then(function (results) {
-           return res.send("OK")
+            let condition = false;
+            if (req.session.usuario != undefined && req.session.usuario.id == results.usuario.id) {
+                condition = true;
+            }
+            return res.render('product', {title : 'Detalle Producto' , productos: results, comentarios: results.comentarios, condition: condition})
            
         }).catch(function(respuestaNegativa) {
             console.log(respuestaNegativa);
         })
-        // return res.render('product', {title : 'Detalle Producto' , productos : db.productos})
+         
     },
     
 
     add: function(req, res){
 
-        db.usuario.findOne()
-        .then(function (results) {
-            return res.send("OK")    
+        if (req.session.usuario != undefined) {
+            return res.render("product-add", {title: "Agregar Producto"})
+        } else {
+            return res.redirect("/users/login")
+        }
+},
 
-        }).catch(function (respuestaNegativa) {
-            console.log(respuestaNegativa);
-        })
+    store: function(req, res) {
+        let form = req.body;
+        let errors = validationResult(req)
 
-        // return res.render('product-add' , {title : 'Agregar Producto' , usuario : db.usuario})
-}};
+        if (errors.isEmpty()) {
+            db.Producto.create(form)
+            .then((result)=>{
+                return res.redirect("/product/id/" + result.id)
+            }).catch((error) => {
+                return console.log(error);
+            })
+        } else {
+            return res.render("product-add", {title: "Register", errors: errors.mapped(), old: req.body})
+        }
+    },
 
+    edit: function(req, res) {
+        let form = req.body;
+
+        let criterio = {
+            include: [
+                {association: "usuario"}
+            ]}
+            db.Producto.findByPk(form.id, criterio)
+            .then(function (results) {
+                return res.render("product-edit", {title: `Editar producto ${results.nombreProducto}`, productos: results})
+            })
+            .catch((error) => {
+                return console.log(error);
+            })
+    },
+
+    update: function(req, res) {
+        
+    },
+
+    delete: function(req, res) {
+        let form = req.body;
+
+        let filtrado = {
+            where: {
+                id: form.id
+            }
+                
+        }
+
+        if (req.session.usuario != undefined) {
+            let id = req.session.usuario.id;
+            if (form.idUsuario == id) {
+                db.Producto.destroy(filtrado)
+                .then((result) => {
+                    return res.redirect("/");
+                }).catch((error) => {
+                    return console.log(error);
+                })
+            } else {
+                return res.redirect("/users/profile/id" + id);
+            }
+        } else {
+            return res.redirect("/users/login");
+        }
+    },
+
+    comment: function(req, res) {
+        let form = req.body;
+        let errors = validationResult(req)
+        if (errors.isEmpty()) {
+            let comentario = {
+                usuarioId: req.session.usuario.id,
+                productoId: req.params.id,
+                comentario: form.comentario
+            }
+
+            db.Comentario.create(comentario)
+            .then((resultados) => {
+                return res.reditect("/product/id/" + req.params.id)
+            }).catch((error)=> {
+                return console.log(error);
+            });
+        }
+        else {
+            let id = req.params.id
+            let condition = false
+            
+            let criterio = {
+                include: [
+                    {association: "usuario"},
+                    {association: "comentarios",
+                include: [{association: "usuario"}
+            ]}
+            ],
+            order: [[{model: db.Comentario, as: "comentarios"}, "createdAt", "DESC"]]
+            }
+            db.Producto.findByPk(id, criterio)
+            .then(function(results) {
+                if (req.session.usuario != undefined && req.session.usuario.id == results.usuario.id) {
+                    condition = true;                   
+                }
+
+                return res.render("producto", {title: "Producto", productos: results, comentarios: results.comentarios, condition: condition }) // errors: errors.mapped(), old: req.body
+                
+                .catch(function(error) {
+                    return console.log(error);
+                })
+            })
+        }
+    }
+}
 module.exports = productocontoller;
--- myapp/routes/product.js
+++ myapp/routes/product.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const productoscontroller = require('../controllers/productoscontroller');
+const {body} = require("express-validator");
+
+let validations = [
+    body("nombreProducto")
+        .notEmpty().withMessage("Este campo es obligatorio."),
+    body("descripcionProducto")
+        .notEmpty().withMessage("Este campo es obligatorio."),
+    body("imagenProducto")
+        .notEmpty().withMessage("Este campo es obligatorio.").bail()
+        .isURL().withMessage("Este campo debe tener una URL valida.")
+]
+
+let validationsComentario = [
+    body("comentario")
+        .notEmpty().withMessage("Este campo no puede estar vacio.").bail()
+        .isLength({min:3}).withMessage("El comentario debe tener al menos 3 caracteres.")
+];
 
 router.get('/', productoscontroller.index);
+router.post("/", validationsComentario, productoscontroller.comment)
 
 router.get('/add', productoscontroller.add);
+router.post("/add", validations, productoscontroller.store);
+
+router.post("/editProduct", productoscontroller.edit);
+router.post("/editar", validations, productoscontroller.update);
+
+router.post("/delete", productoscontroller.delete)
 
 module.exports = router;
