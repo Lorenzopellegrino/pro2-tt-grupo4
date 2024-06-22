8ba27b02097440ddf94b93d2249b2a4e91c6356a
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2024-06-22 20:15:21 -0300

    route

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const userscontroller = require('../controllers/users');
+const {body} = require("express-validator");
+const db = require ("../database/models");
+const session = require('express-session');
+const bcrypt = require("bcryptjs");
+
+let validationsLogin = [
+    body('email')
+    .notEmpty().withMessage('Este campo es obligatorio. ').bail()
+    .isEmail().withMessage('Tiene que tener rquisitos de email').bail()
+    .custom(function(value, {req})){
+        return db.Usuario.findOne({where: {mail: req.body.email},})
+    }   .then(function(user){
+            if(user != undefined){
+                return true;
+            }
+            else{
+                throw new Error ('no existe')
+            }
+
+        })
+    
+]
 
 router.get('/login', userscontroller.login);
 
